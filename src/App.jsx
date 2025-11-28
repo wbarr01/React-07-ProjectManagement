8baec0d0264264f9107894dcae8f3b3ba2603629
Walter Barrantes <wbarrantesrios@gmail.com>
2025-11-27 20:35:46 -0500

    adding form createProject and update sidebarlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
 import { useState } from 'react';
+import noProjectImg from './assets/no-projects.png';
 import ProjectForm from "./components/ProjectForm";
 
+const INIT_PROJECT_LIST = [];
 function App() {
   const [isCreating, setIsCreating] = useState(false);
+  const [projectList, setProjectList] = useState(INIT_PROJECT_LIST);
 
   function handleClick() {
     setIsCreating(prevIsCreating => !prevIsCreating);
   }
-  let myForm = <ProjectForm handleClick={handleClick} />;
-  if (isCreating) myForm = <p>my love Vanessa</p>
+
+  function addNewProject(newProject) {
+    setProjectList(prev => ([...prev, newProject]));
+    console.log(projectList);
+  }
+
+  let myForm = (
+    <div className="gap-3">
+      <img className="h-24 w-24" src={noProjectImg} alt="No Projects image" />
+      <h1>No Project Selected</h1>
+      <p>Select a project ot get started withy a new one</p>
+      <button id="NewProject" onClick={handleClick} className="bg-gray-900 text-white"> Create new Project</button>
+    </div>);
+  if (isCreating) myForm = <ProjectForm addNewProject={addNewProject} />
 
 
   return (
     <>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-      <aside id="projectSideBar" className="bg-black min-w-24 max-w-40 text-white">
+      <aside id="projectSideBar" className="bg-black min-w-24 max-w-40 min-h-64 text-white float-left">
         <h2>Your Projects</h2>
-        <button id="AddProject" onClick={handleClick} className="bg-gray-600"> +Add Project</button>
+        <button id="AddProject" onClick={handleClick} className="bg-gray-600 "> +Add Project</button>
+        <ul>
+          {projectList.map((p, index) => (<li key={index}>{p.title}</li>))}
+        </ul>
       </aside>
       {myForm}
     </>
--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,11 +1,42 @@
-import noProjectImg from '../assets/no-projects.png';
+import { useState } from 'react';
+const INIT_PROJECT = {
+    title: "",
+    description: "",
+};
 
-export default function ProjectForm({ handleClick }) {
+export default function ProjectForm({ addNewProject }) {
+    const [project, setProject] = useState(INIT_PROJECT);
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setProject(prevProject => ({
+            ...prevProject,
+            [name]: value,
+        }));
+    }
+
+    function handleCancel() {
+        setProject(INIT_PROJECT);
+    }
+
+    function handleSave() {
+        if (project != null && project.title !== "") {
+            addNewProject(project);
+            setProject(INIT_PROJECT);
+        }
+    }
+
 
     return (
-        <><img className="h-24 w-24" src={noProjectImg} alt="No Projects image" />
-            <h1>No Project Selected</h1>
-            <p>Select a project ot get started withy a new one</p>
-            <button id="NewProject" onClick={handleClick} className="bg-gray-900 text-white"> Create new Project</button></>
+        <form className="">
+            <h1>{project.title}  {project.description}</h1>
+            <button id="cancel" onClick={handleCancel} className="bg-gray-300"> cancel</button>
+            <button id="save" onClick={handleSave} className="bg-gray-900 text-white"> Save</button>
+            <h2>TITLE</h2>
+            <input id="title" name="title" type='text' required value={project.title} onChange={handleChange} className="bg-gray-300"></input>
+            <h2>DESCRIPTION</h2>
+            <input id="description" name="description" type='text' value={project.description} onChange={handleChange} className="bg-gray-300"></input>
+            <h2>DUE DATE</h2>
+        </form>
     );
 }
